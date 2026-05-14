a6674aee9c67a9b4f487b4977b22c3a4574acef1
MasettoJr <paulomasetto93@gmail.com>
2026-05-14 20:34:39 -0300

    Linhas em branco removidas

--- app.py
+++ app.py
@@ -2,18 +2,11 @@ import streamlit as st
 import sqlite3
 from time import sleep
 
- 
-
- 
 # Cria conexão com o banco
 conexao = sqlite3.connect("banco.db")
 
- 
-
 # Mensageiro entre Python e banco
 cursor = conexao.cursor()
-
- 
 
 # Define estrutura da tabela
 cursor.execute("""CREATE TABLE IF NOT EXISTS avaliacoes (
@@ -28,43 +21,31 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS avaliacoes (
 
                 )""")
 
- 
-
 # Widgets 
 
 # Flag para limpar campos
 if "limpar_formulario" not in st.session_state:
     st.session_state["limpar_formulario"] = False
 
- 
-
 # Flag para exibir success box
 if "msg_sucesso" in st.session_state:
     st.success(st.session_state["msg_sucesso"])
     del st.session_state["msg_sucesso"]
 
- 
-
 if st.session_state["limpar_formulario"]:
     st.session_state["comentario"] = ""
     st.session_state["opcao"] = ""
     st.session_state["limpar_formulario"] = False
-
  
 st.title("Avalie sua experiência")
 
- 
 # Opções
 choice = st.radio("Qualidade do atendimento", ["Ruim", "Regular", "Bom", "Ótimo"], index=None, key="opcao")
 
 # Campo de texto livre
 text = st.text_area("Gostaria de deixar um comentário?", key="comentario")
 
- 
-
 clicked = st.button("Enviar")
-
- 
 
 if clicked:
     # Insere dados coletados na tabela
